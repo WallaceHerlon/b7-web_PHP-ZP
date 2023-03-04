e3e808e5d4e8825aa125630318d995cdfa8ae175
Wallace Herlon Silva <wherlon@hotmail.com>
2023-03-03 22:32:42 -0300

    Aula PHP intermediário - aula 4
    Funções matemáticas, Manipulação de textos.

---
+++ 09_PHP/intermediario/07_funcoes_mat.php
@@ -0,0 +1,13 @@
+<?php
+
+echo abs(-10);
+echo "<br/>";
+echo round(3.7);
+echo "<br/>";
+echo ceil(3.1);
+echo "<br/>";
+echo floor(2.9);
+echo "<br/>";
+echo rand(0, 10);
+
+?>
---
+++ 09_PHP/intermediario/08_manipulacao_textos.php
@@ -0,0 +1,30 @@
+<?php
+
+//$nome = "Wallace Herlon";
+//$x = explode(" ", $nome);
+//print_r($x);
+
+//$array = array("Wallace", "Herlon");
+//$nomeCompleto = implode(" ", $array);
+//echo $nomeCompleto;
+
+//$x = number_format(8.285948384, 2);
+//$x = number_format(34567.285384, 3, ",", ".");
+//echo $x;
+
+//$texto = "O rato roeu a roupa!";
+//$string = str_replace("roeu", "comeu", $texto);
+//echo $string;
+
+//echo strtolower("WALLACE HERLON");
+//echo strtoupper("wallace herlon");
+
+//$texto = "Wallace";
+//$x = substr($texto, 0, 3);
+//echo $x;
+
+$nome = "wallace herlon silva";
+echo ucfirst($nome);
+echo "<br/>";
+echo ucwords($nome);
+?>
